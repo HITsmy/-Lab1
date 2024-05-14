0715e8608b5bca6c34a56165e3bc337d0ef10bc6
tenrijinyoku <132194603+tenrijinyoku@users.noreply.github.com>
2024-05-15 01:21:45 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,12 @@
-package org.example;
+package org.example.Tool;
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.MultiGraph;
-import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.ui.layout.Layouts;
 import org.graphstream.ui.swingViewer.Viewer;
 
 
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException, AWTException {
@@ -21,20 +17,30 @@ public class Main {
 
         // 读取文件并切分字符串得到单词数组
         StringBuilder textContent = Util.read("src/main/java/org/example/text/text.txt");
+        System.out.println(textContent);
         String[] words = textContent.toString().split( "\\s+");
         for (String word : words) {
-            System.out.println(word);
+           // System.out.println(word);
         }
 
         // 根据单词数组生成有向图
         graph = GraphOperations.generateGraph(words);
         System.out.println();
-
+        Util util = new Util();
         // 初始化工具
-        Util.InitTools(GraphOperations.nodeHashMap.keySet(), GraphOperations.edgeHashMap.keySet(), graph);
+        util.InitTools(GraphOperations.nodeHashMap.keySet(), GraphOperations.edgeHashMap.keySet(), graph);
+
+        //GraphOperations.searchWord(util, "strange");
+        //GraphOperations.queryBridgeWords(util, "to","strange");
+        //System.out.println(GraphOperations.generateNewText("TTo strange worlds to seek out life new",util));
+        //System.out.println(GraphOperations.randomWalk(util,"test1.txt"));
+        GraphOperations.calcShortestPath(util,"explore","new");
 
         // 展示有向图
         Viewer viewer = graph.display();
+
+
+
         // 使用图布局算法
         viewer.enableAutoLayout(Layouts.newLayoutAlgorithm());
         Util.draw(viewer,"src/main/java/org/example/png/graph.png");
